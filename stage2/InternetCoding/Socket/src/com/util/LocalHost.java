805a617247aad5173a1462916b72855999941048
huanggg <huanggg@vzoom.com>
2020-01-17 15:15:37 +0800

    add a project

---
+++ stage2/InternetCoding/Socket/src/com/util/LocalHost.java
@@ -0,0 +1,13 @@
+package com.util;
+import java.net.InetAddress;
+public class LocalHost
+{
+	public LocalHost()
+	{
+		
+	}
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ stage2/InternetCoding/Socket/src/com/util/SocketTest.java
@@ -0,0 +1,13 @@
+package com.util;
+public class SocketTest
+{
+	
+	public SocketTest()
+	{
+		
+	}
+	public static void main(String[] args)
+	{
+		
+	}
+}
