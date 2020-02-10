4fe94a7b57acb30021aca2bc915079a1198e2a93
dawangbaixiaofu <dawangbaixiaofu.outlook.com>
2020-02-10 21:35:32 +0800

    commit by intelij idea

---
+++ src/org/hoover/project1/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.hoover.project1;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello,World!");
+    }
+}
--- stage1/HelloWorld.java
+++ stage1/HelloWorld.java
@@ -1,3 +1,4 @@
+package com.hoover.stage1;
 public class HelloWorld
 {
 	public static void main(String[] args){
